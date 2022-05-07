432cab0b5cb95d4f228bf036da4971c839b3051f
Bibko Olha <fahnencazevuolasfanas@protonmail.com>
2022-05-07 12:49:03 +0000

    Finish Serializers

--- blango/settings.py
+++ blango/settings.py
@@ -131,6 +131,7 @@ class Dev(Configuration):
       "allauth.account",
       "allauth.socialaccount",
       "allauth.socialaccount.providers.google",
+      'rest_framework',
   ]
 
   MIDDLEWARE = [
